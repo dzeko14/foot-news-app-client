fe249a2497826743c2dc4ea16b9cb9480ba7f641
Евгений <dzekomerden@gmail.com>
2019-03-17 23:43:16 +0200

    Handle navigation view back arrow

--- app/src/main/java/my/dzeko/footapp/view/activity/MainActivity.kt
+++ app/src/main/java/my/dzeko/footapp/view/activity/MainActivity.kt
@@ -3,7 +3,10 @@ package my.dzeko.footapp.view.activity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import my.dzeko.footapp.R
 import my.dzeko.footapp.listener.BottomNavViewVisibilityListener
@@ -17,24 +20,35 @@ class MainActivity : DaggerAppCompatActivity(), MainView {
     @Inject lateinit var mPresenter: MainPresenter
 
     private lateinit var mBottomNavView: BottomNavigationView
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mPresenter.subscribe(this)
 
-        val navController = findNavController(R.id.main_container)
+        mNavController = findNavController(R.id.main_container)
 
+        //Set up navigation bar
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.newsListFragment,
+            R.id.userNewsListFragment, R.id.splashScreenFragment))
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+
+        //Set up bottom navigation view
         mBottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
             .apply {
-            val listener = BottomNavigationViewItemsListener(navController)
+            val listener = BottomNavigationViewItemsListener(mNavController)
             selectedItemId = listener.lastClickedItem
             setOnNavigationItemSelectedListener(listener)
         }
 
-        navController.addOnDestinationChangedListener(
+        mNavController.addOnDestinationChangedListener(
             BottomNavViewVisibilityListener(mPresenter::onBottomNavViewVisibility)
         )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return mNavController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onDestroy() {
