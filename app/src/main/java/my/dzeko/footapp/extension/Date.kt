1c00c00dfece7498ab24bfc56529dc5ce8683581
Евгений <dzekomerden@gmail.com>
2019-03-30 18:52:03 +0200

    Fix bug with formatting date output in news list

--- app/src/main/java/my/dzeko/footapp/extension/Date.kt
+++ app/src/main/java/my/dzeko/footapp/extension/Date.kt
@@ -11,13 +11,25 @@ private const val HOUR_IN_MILLS = 3_600_000
 fun Date.formattedTime(resource: Resources): String {
     val diff = Date().time - this.time
     val calendar = Calendar.getInstance()
+    val currentCalendar = Calendar.getInstance()
     calendar.time = this
+    currentCalendar.time = Date()
     val minutes = calendar.get(Calendar.MINUTE)
     val hours = calendar.get(Calendar.HOUR_OF_DAY)
     return when {
-        diff <= HOUR_IN_MILLS -> resource.getQuantityString(R.plurals.minutes, minutes, minutes)
+        diff <= HOUR_IN_MILLS -> {
+            val hDiff = currentCalendar.get(Calendar.MINUTE) - minutes
+            resource.getQuantityString(R.plurals.minutes,
+                hDiff,
+                hDiff)
+        }
 
-        diff <= DAY_IN_MILLS -> resource.getQuantityString(R.plurals.hours, hours, hours)
+        diff <= DAY_IN_MILLS -> {
+            val mDiff = currentCalendar.get(Calendar.HOUR_OF_DAY) - hours
+            resource.getQuantityString(R.plurals.hours,
+                mDiff,
+                mDiff)
+        }
 
         else -> {
             val formatter = SimpleDateFormat("dd-MM-YYYY HH:mm", Locale.getDefault())
